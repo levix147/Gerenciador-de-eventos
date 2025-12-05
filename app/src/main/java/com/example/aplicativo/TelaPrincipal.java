deb67ff8f756e3d5d7963899888974fca41895b8
levix147 <levihenrycor@gmail.com>
2025-12-05 19:45:48 -0300

    inicial

---
+++ app/src/main/java/com/example/aplicativo/TelaPrincipal.java
@@ -0,0 +1,40 @@
+package com.example.aplicativo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_principal);
+
+        IniciarComponentes();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        text_tela_cadastro.setOnClickListener(v -> {
+            startActivity(new Intent(TelaPrincipal.this, FormCadastro.class));
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_telacadastro);
+    }
+}
